0091364cdad38658701f028729c344d06219b5ef
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-03-12 17:42:44 -0300

    trying to fix the API problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-
+import React, {useState, useEffect} from "react";
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('')
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  })
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <h1 className="text-amber-400">Testing</h1>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"></img>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl">Photo by Jhon Doe</div>
+      <ul>
+        <li>
+          <strong>Views: </strong>
+          4000
+        </li>
+        <li>
+          <strong>Downloads: </strong>
+          1500
+        </li>
+        <li>
+          <strong>Views: </strong>
+          400
+        </li>
+      </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+      </div>
     </div>
   );
 }
