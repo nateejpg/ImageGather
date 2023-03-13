687107ae58fe736a447646bb1735b0d5643c582a
Nathan <nathan11.abreu@gmail.com>
2023-03-13 18:40:44 -0300

    adding the props of thje component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { ImageCard } from "./components/ImageCard";
 
 function App() {
 
@@ -6,42 +7,24 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('')
 
-  // JUST ADDING A COMMENT HERE TO CHECK IF IT WORKED
-  // I guess we did it
-
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      setImages(data.hits)
+      setIsLoading(false)
+    })
     .catch(err => console.log(err))
   });
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full"></img>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl">Photo by Jhon Doe</div>
-      <ul>
-        <li>
-          <strong>Views: </strong>
-          4000
-        </li>
-        <li>
-          <strong>Downloads: </strong>
-          1500
-        </li>
-        <li>
-          <strong>Views: </strong>
-          400
-        </li>
-      </ul>
+     <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
       </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
-      </div>
-    </div>
+     </div>
   );
 }
 
