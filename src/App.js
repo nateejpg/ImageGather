0765b4318f14d54ba6753edce77f295e5ab8d463
Nathan <nathan11.abreu@gmail.com>
2023-03-13 22:12:01 -0300

    Finishing the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { ImageCard } from "./components/ImageCard";
+import { ImageSearch } from "./components/ImageSearch";
 
 function App() {
 
@@ -15,15 +16,16 @@ function App() {
       setIsLoading(false)
     })
     .catch(err => console.log(err))
-  });
+  },[term]);
 
   return (
      <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+      <ImageSearch searchText = {(text) => setTerm(text)}/>
+      {isLoading ? <h1 className="text-6xl text-center.mx-auto mt-32">Loading...</h1>: <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image}/>
         ))}
-      </div>
+      </div>}
      </div>
   );
 }
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export const ImageCard = ({image}) => {
+
+  const tags = image.tags.split(',');
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
     <img src={image.webformatURL} alt="" className="w-full"></img>
@@ -22,10 +25,13 @@ export const ImageCard = ({image}) => {
     </ul>
     </div>
     <div className="px-6 py-4">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+      {tags.map((tag, index) => (
+        <span key = {index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{tag}</span>
+      ))}
     </div>
   </div>
   )
 }
+
+
+
